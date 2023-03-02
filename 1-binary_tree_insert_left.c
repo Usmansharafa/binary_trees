625d61c4a5e92b940f3ec5a0cf47a97044cd712b
Usman Sharafa <Usmansharafa04@gmail.com>
2023-03-02 01:51:20 +0100

    Task 1 solved

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,6 +11,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *new;
 	binary_tree_t *tmp;
 
+	if (!parent)
+		return (NULL);
 	if (parent->left)
 	{
 		tmp = parent->left;
